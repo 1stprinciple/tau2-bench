2c4ce65fc06c800f819f1a0d2c9b30f84c49d725
1stprinciple <ztyharbin@gmail.com>
2025-08-10 10:21:12 +0800

    hosted_vllm

--- src/tau2/utils/llm_utils.py
+++ src/tau2/utils/llm_utils.py
@@ -203,7 +203,7 @@ def generate(
         kwargs["thinking"] = {"type": "disabled"}
     elif "rlor" in model:
         kwargs["base_url"] = "http://localhost:11434/v1"
-        kwargs["custom_llm_provider"] = "openai"
+        kwargs["custom_llm_provider"] = "hosted_vllm"
         del kwargs["seed"]
     elif "fireworks" in model:
         kwargs["base_url"] = "https://api.fireworks.ai/inference/v1"
